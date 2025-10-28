d8ab3b873b9894b408f0f766dc914c06ce859f1d
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-10-29 00:17:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/profile", profileRoutes);
-app.use("/api",userRoutes);
+app.use("/api/user",userRoutes);
 
 
 app.use("/api/logo", avatarRoutes);
