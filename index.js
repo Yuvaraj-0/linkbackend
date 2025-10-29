c49d7845db499b71d73f81ccc7e419b08f37c502
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-10-29 12:54:08 +0530

    Fix allowedOrigins entry for linkb-puce

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   "http://localhost:5173", // local React dev server
   "https://linkb-kktodbgzn-yuvaraj-0s-projects.vercel.app",
  "https://linkb-git-main-yuvaraj-0s-projects.vercel.app",
-  "https://linkb-puce.vercel.app/"
+  "https://linkb-puce.vercel.app"
 
 ];
 
